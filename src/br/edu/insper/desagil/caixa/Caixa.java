d14b16e12ad48846264ae49c36157bb50ed18c97
Gabriel Couto <38431521+gabrielcla@users.noreply.github.com>
2021-03-21 14:23:21 -0300

    Classes Caixa e Carrinho

--- src/br/edu/insper/desagil/caixa/Caixa.java
+++ src/br/edu/insper/desagil/caixa/Caixa.java
@@ -1,5 +1,54 @@
 package br.edu.insper.desagil.caixa;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.edu.insper.desagil.carrinho.Carrinho;
+import br.edu.insper.desagil.pedido.Pedido;
+import br.edu.insper.desagil.produto.Produto;
+
 public class Caixa {
+	// Mapeia código de produtos para inteiros (1 a 99)
+    private Map<Integer, Integer> dicDescontos;
+
+	public Caixa() {
+		Map<Integer,Integer> d = new HashMap<>();
+		this.dicDescontos = d;
+	}
+	
+    public void Adicionar(Produto produto, int desconto) {
+    	int codProduto = produto.getCodigo();
+    	
+    	// Se codigo estiver entre 1 e 99 insere o código do produto no dicionario com o seu desconto
+    	if (desconto>=1 && desconto<=99) {
+    		this.dicDescontos.put(codProduto, desconto);
+    	}
+    }
+    
+    public double valorTotal(Carrinho carrinho) {
+    	double valorFinal = 0;
+    	
+    	// Lista de pedidos no carrinho
+    	List<Pedido> lPedidos = carrinho.getPedidos();
+    	
+    	// Para cada um dos pedidos no carrinho
+    	for (Pedido pedido: lPedidos) {
+    		int codProduto = pedido.getProduto().getCodigo(); // Código do produto do pedido
+    		double valorPedido = pedido.totalPedido(); // Valor do pedido
+    		
+    		int desconto = 0;
+    		// Se o código do produto estiver no dicDescontos
+    		if (this.dicDescontos.containsKey(codProduto)) {
+    			desconto = this.dicDescontos.get(codProduto); // pega o valor do desconto 
+    		}
+    		
+    		double valorFinalPedido = valorPedido *(1-desconto/100); // Valor do pedido com desconto
+    		valorFinal = valorFinal + valorFinalPedido; // Acrescenta o valor ao valor final
+        		
+    	}
+    	
+    	return valorFinal;
+    }
 
 }
--- src/br/edu/insper/desagil/carrinho/Carrinho.java
+++ src/br/edu/insper/desagil/carrinho/Carrinho.java
@@ -1,5 +1,50 @@
 package br.edu.insper.desagil.carrinho;
 
-public class Carrinho {
+import java.util.ArrayList;
+import java.util.List;
 
+import br.edu.insper.desagil.pedido.Pedido;
+import br.edu.insper.desagil.produto.Produto;
+
+
+public class Carrinho {
+	private List<Pedido> pedidos;
+
+	public Carrinho() {
+		this.pedidos = new ArrayList<Pedido>();
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+	
+	public void recebeProduto(Produto produto) {
+		List<Pedido> l = getPedidos(); // Pega a lista com todos os pedidos no carrinho
+		boolean noCarrinho = false;
+		
+		// Para cada pedido no carrinho
+		for (int i = 0; i < l.size(); i++) {
+		    Pedido ped = l.get(i); // Pedido do carrinho
+		    Produto prod = ped.getProduto(); // Produto do pedido no carrinho  
+		    // Se o produto recebido já for um dos produtos no carrinho
+		    if (prod == produto) {
+		    	ped.incrementaQuantidade(); // Incrementa quantidade desse produto no pedido dele
+		    	noCarrinho = true;
+		    }
+		}
+		
+		// Se o produto não estiver no carrinho
+		if (noCarrinho == false) {
+			// Constroi um pedido
+			Pedido pedido = new Pedido(produto);
+			
+			// Insere o pedido no carrinho
+			this.pedidos.add(pedido);
+		}
+	
+		
+		
+	}
+	
+	
 }
